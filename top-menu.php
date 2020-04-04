b6da239a4effd91c297932ea4bd96379f2faa36b
anwaryazid <m.anwaryazid@gmail.com>
2020-04-05 00:22:27 +0800

    Update top-menu.php

--- top-menu.php
+++ top-menu.php
@@ -1,5 +1,5 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="search.php">UmrahClicks.my</a>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light shadow-sm" style="background-color: white;">
+  <a class="navbar-brand text-primary mb-0 h1" href="search.php">UmrahClicks.my</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -14,16 +14,16 @@
       <a class="nav-item nav-link" href="#" data-toggle="modal" data-target="#contactModal">Contact</a>           
     </div>    
   </div>
-  <div class="float-right text-white">
+  <div class="float-right" style="font-size: .8rem;">
     <form class="form-inline">
       Date Depart From&nbsp;
-      <input type="text" class="form-control form-control-sm bg-primary text-white" id="date_Depart" value="28/03/2020" size="10">&nbsp;
+      <input type="text" class="form-control form-control-sm" id="date_Depart" value="28/03/2020" size="10">&nbsp;
       Adult&nbsp;
-      <input type="text" class="form-control form-control-sm bg-primary text-white" id="no_adult" value="3" size="1">&nbsp;
+      <input type="text" class="form-control form-control-sm" id="no_adult" value="3" size="1">&nbsp;
       Children&nbsp;
-      <input type="text" class="form-control form-control-sm bg-primary text-white" id="no_children"  value="3" size="1">
+      <input type="text" class="form-control form-control-sm" id="no_children"  value="3" size="1">
       &nbsp;&nbsp;&nbsp;&nbsp;
-      <button class="btn btn-outline-light my-2 my-sm-0 btn-sm" type="submit"><i class="fas fa-search fa-sm"></i>&nbsp;Search</button>
+      <button class="btn btn-outline-primary my-2 my-sm-0 btn-sm" type="submit"><i class="fas fa-search fa-sm"></i>&nbsp;Search</button>
     </form>
   </div> 
 </nav>
