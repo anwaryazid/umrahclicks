22383c36a9b0a3904790e1e97afdd12f31f7da5b
anwaryazid <m.anwaryazid@gmail.com>
2020-04-10 06:18:33 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,15 +1,13 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-white sidebar sidebar-light accordion" id="accordionSidebar" style="border-right: solid 1px lightgrey;">
 
-  <!-- Sidebar - Brand -->
   <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
     <!-- <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-laugh-wink"></i>
     </div> -->
-    <div class="sidebar-brand-text mx-3">UmrahClicks.my</div>
+    <div class="sidebar-brand-text mx-3 text-primary">UmrahClicks.my</div>
   </a>
 
-  <!-- Divider -->
-  <hr class="sidebar-divider my-0">
+  <hr class="sidebar-divider my-0"> 
 
   <?php
     if (isset($_GET['page'])) {
@@ -18,6 +16,7 @@
       $page = 'dashboard';
     }          
   ?>
+
   <li class="nav-item <?php if ($page == 'dashboard'){ ?>active<?php } ?>">
     <a class="nav-link" href="index.php?page=dashboard">
       <i class="fa fa-fw fa-tachometer-alt"></i>
@@ -58,7 +57,7 @@
 
   <li class="nav-item <?php if ($page == 'user-management'){ ?>active<?php } ?>">
     <a class="nav-link" href="index.php?page=user-management">
-      <i class="fa fa-fw fa-user"></i>
+      <i class="fa fa-fw fa-users"></i>
       <span>User Management</span></a>
   </li>
 
@@ -75,7 +74,6 @@
     </a>
     <div id="refData" class="collapse <?php if (strpos($page, 'ref') !== false){ ?>show<?php } else { ?>hide<?php } ?>" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Data Reference:</h6>
         <!-- <a class="collapse-item <?php if ($page == 'ref-user'){ ?>active<?php } ?>" href="index.php?page=ref-user">User Type</a> -->
         <!-- <a class="collapse-item <?php if ($page == 'ref-group'){ ?>active<?php } ?>" href="index.php?page=ref-group">Group Type</a> -->
         <a class="collapse-item <?php if ($page == 'ref-country'){ ?>active<?php } ?>" href="index.php?page=ref-country">Country</a>
@@ -83,12 +81,23 @@
     </div>
   </li>
 
-  <!-- Divider -->
-  <hr class="sidebar-divider d-none d-md-block">
+  <!-- <hr class="sidebar-divider my-0"> -->
+
+  <!-- <li class="nav-item">
+    <a class="nav-link" href="#" data-toggle="modal" data-target="#profileModal">
+      <i class="fa fa-fw fa-user"></i>
+      <span>Profile</span></a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
+      <i class="fa fa-fw fa-sign-out-alt"></i>
+      <span>Logout</span></a>
+  </li> -->
 
   <!-- Sidebar Toggler (Sidebar) -->
-  <div class="text-center d-none d-md-inline">
+  <!-- <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
-  </div>
+  </div> -->
 
 </ul>
