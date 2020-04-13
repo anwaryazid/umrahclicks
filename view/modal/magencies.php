244343c10d0abc05aefd8817e9eb727688ee7dc9
anwaryazid <m.anwaryazid@gmail.com>
2020-04-14 00:10:56 +0800

    Update magencies.php

--- view/modal/magencies.php
+++ view/modal/magencies.php
@@ -7,95 +7,32 @@
           <span aria-hidden="true">×</span>
         </button>
       </div>
-      <div class="modal-body" style="height: 480px; overflow-y: auto; font-size:.8rem;">
+      <div class="modal-body" style="max-height: 480px; overflow-y: auto; font-size:.8rem;">
         <div class="row">
           <div class="col-md-8">
-            <h6 class="font-weight-bold text-primary">Al-Nile Tour & Travel Sdn Bhd</h6> 
-            LKU No. : 6043<br>
-            No 8, Kompleks Perniagaan Hidayah, Jalan Hutan Kampung 05350 Alor Setar<br>
-            TEL : +604-733 4023/733 4024<br> 
-            EMAIL : alnile_travel@yahoo.com
+            <h6 class="font-weight-bold text-primary">Smart Umrah4all Dot Com Travel & Services Sdn Bhd</h6> 
+            LKU No : KPK/LN - 9774<br>
+            <span class="text-capitalize">F1-18-10, TAMARIND SUITES, PERSIARAN MULTIMEDIA, 63000 CYBERJAYA, MALAYSIA</span><br>
+            TEL : +603-8230 8076<br> 
+            EMAIL : ​sales@umrahclicks.com
           </div>
           <div class="col-md-4" style="text-align: center;">
-            <img src="img/logo-agency/alnile.jpg" style="max-height:90px; max-width:100%;">
+            <img src="img/logo-agency/smart.png" style="max-height:90px; max-width:100%;">
           </div>
         </div>
         <hr>
         <div class="row">
           <div class="col-md-8">
-            <h6 class="font-weight-bold text-primary">Ash-Har Travel & Tours Sdn Bhd</h6> 
-            LKU No. : 6043<br>
-            No. 23, 1, Jalan 3/76d, Desa Pandan, 55100 Kuala Lumpur, Wilayah Persekutuan<br>
-            TEL : +603-9200 7050<br> 
-            EMAIL : ashhartravel@gmail.com
+            <h6 class="font-weight-bold text-primary">EPL Travel and Tours Sdn Bhd</h6> 
+            LKU No : KP/LN 6441<br>
+            TEL : +6019.993.9723 (Lutfi)0<br> 
+            EMAIL : ask.epltravel@gmail.com 
           </div>
           <div class="col-md-4" style="text-align: center;">
-            <img src="img/logo-agency/ashhar.png" style="max-height:90px; max-width:100%;">
+            <img src="img/logo-agency/epl.png" style="max-height:90px; max-width:100%;">
           </div>
         </div>
-        <hr>
-        <div class="row">
-          <div class="col-md-8">
-            <h6 class="font-weight-bold text-primary">Andalusia Travel & Tours Sdn Bhd</h6> 
-            LKU No. : 6043<br>
-            25, 27, 29, Jalan 1, 76, Jalan Kampung Pandan, Desa Pandan, 55100 Kuala Lumpur<br>
-            TEL : +603-9283 1588<br> 
-            EMAIL : umrah@andalusia2u.com
-          </div>
-          <div class="col-md-4" style="text-align: center;">
-            <img src="img/logo-agency/andalusia.png" style="max-height:90px; max-width:100%;">
-          </div>
-        </div>
-        <hr>
-        <!-- <div class="row">
-          <div class="col-md-12">
-            <div class="card shadow mb-4">
-              <a href="#lagency_1" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="lagency_1">
-                <div class="row">
-                  <div class="col-lg-9">
-                    <h6 class="m-0 font-weight-bold">Al-Nile Tour & Travel Sdn Bhd</h6> 
-                    Puchong (LKU No : 6044)
-                  </div>
-                </div>                   
-              </a>
-              <div class="collapse hide" id="lagency_1">
-                <div class="card-body">
-                  
-                </div>
-              </div>
-            </div>
-            <div class="card shadow mb-4">
-              <a href="#lagency_2" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="lagency_2">
-                <div class="row">
-                  <div class="col-lg-9">
-                    <h6 class="m-0 font-weight-bold">Ash-Har Travel & Tours Sdn Bhd</h6> 
-                    Damansara (LKU No : 6044)
-                  </div>
-                </div>                   
-              </a>
-              <div class="collapse hide" id="lagency_2">
-                <div class="card-body">
-                  
-                </div>
-              </div>
-            </div>
-            <div class="card shadow mb-4">
-              <a href="#lagency_3" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="lagency_3">
-                <div class="row">
-                  <div class="col-lg-9">
-                    <h6 class="m-0 font-weight-bold">Andalusia Travel & Tours Sdn Bhd</h6> 
-                    Klang (LKU No : 6044)
-                  </div>
-                </div>                   
-              </a>
-              <div class="collapse hide" id="lagency_3">
-                <div class="card-body">
-                  
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>  -->         
+        <hr>       
       </div>
       <div class="modal-footer">
         <button class="btn btn-outline-secondary btn-sm" type="button" data-dismiss="modal">Back</button>
