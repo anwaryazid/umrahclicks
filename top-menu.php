0cb8817a850eefcccdb6f1ebd7aadc2aca132563
anwaryazid <m.anwaryazid@gmail.com>
2020-04-14 02:05:27 +0800

    Update top-menu.php

--- top-menu.php
+++ top-menu.php
@@ -1,5 +1,5 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-light shadow-sm bg-white">
-  <a class="navbar-brand text-primary mb-0" href="home.php"><i class="fas fa-fw fa-kaaba text-gray-900"></i>&nbsp;UmrahClicks.my</a>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary shadow-sm">
+  <a class="navbar-brand mb-0" href="home.php"><i class="fas fa-fw fa-kaaba"></i>&nbsp;UmrahClicks.my</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -16,20 +16,20 @@
       </div>    
     </div>
     <hr>
-    <div class="float-lg-right" style="font-size: .8rem; float: right;">
+    <div class="float-lg-right text-white" style="font-size: .8rem; float: right;">
       <form class="form-inline">
         Date Depart From&nbsp;
-        <input type="text" class="form-control form-control-sm" id="date_Depart" value="28/03/2020" size="10">&nbsp;
+        <input type="text" class="form-control form-control-xs bg-primary text-white text-center" id="date_Depart" value="28/03/2020" size="10">&nbsp;
         Adult&nbsp;
-        <input type="text" class="form-control form-control-sm" id="no_adult" value="3" size="1">&nbsp;
+        <input type="text" class="form-control form-control-xs bg-primary text-white text-center" id="no_adult" value="3" size="1">&nbsp;
         Children&nbsp;
-        <input type="text" class="form-control form-control-sm" id="no_children"  value="3" size="1">
+        <input type="text" class="form-control form-control-xs bg-primary text-white text-center" id="no_children"  value="3" size="1">
         <span class="d-none d-sm-block">&nbsp;&nbsp;&nbsp;&nbsp;</span>
-        <button class="btn btn-outline-primary my-2 my-sm-0 btn-sm" type="submit"><i class="fas fa-search fa-sm"></i>&nbsp;Search</button>
+        <button class="btn btn-outline-light my-2 my-sm-0 btn-sm" type="submit">&nbsp;<i class="fas fa-search fa-sm"></i>&nbsp;Search&nbsp;</button>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <img src="img/flag/MY.png" height="20" alt="">
         &nbsp;
-        <span class="font-weight-bold text-primary">MYR</span>
+        <span class="font-weight-bold">MYR</span>
       </form>
   </div>  
   </div>   
