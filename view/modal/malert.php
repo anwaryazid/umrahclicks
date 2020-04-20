9b0c46cb522f13edc3e2a08b4bd92786f5eb6268
anwaryazid <m.anwaryazid@gmail.com>
2020-04-21 00:38:34 +0800

    Update alert

--- view/modal/malert.php
+++ view/modal/malert.php
@@ -1,5 +1,5 @@
 <div class="alert alert-success collapse" id="success-alert" style="font-size: 0.8rem;">
-  <div class="fa fa-thumbs-up"></div>
+  <div class="fa fa-check fa-sm"></div>
   <strong>Success!</strong> <span id="textAlertSuccess"></span>&nbsp;&nbsp;&nbsp;
   <button type="button" class="close" onClick="closeAlert('success-alert');">
     <span aria-hidden="true">&times;</span>
@@ -7,7 +7,7 @@
 </div>
 
 <div class="alert alert-danger collapse" id="danger-alert" style="font-size: 0.8rem;">
-  <div class="fa fa-thumbs-down"></div>
+  <div class="fa fa-times fa-sm"></div>
   <strong>Unsuccessful!</strong> <span id="textAlertDanger"></span>&nbsp;&nbsp;&nbsp;
   <button type="button" class="close" onClick="closeAlert('danger-alert');">
     <span aria-hidden="true">&times;</span>
