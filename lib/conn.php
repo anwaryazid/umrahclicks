df7b78c711698781cea1d43beede4433262cd5d8
anwaryazid <m.anwaryazid@gmail.com>
2020-05-01 02:36:13 +0800

    Update conn.php

--- lib/conn.php
+++ lib/conn.php
@@ -1,16 +1,16 @@
 <?php
 
-// $username = "umrahcli_ucmy";
-// $password = "7YCMmC6hry6qjEes";
-// $host = "umrahclicks.com";
-// $dbname = "umrahcli_umrahclicks";
+$username = "umrahcli_ucmy";
+$password = "7YCMmC6hry6qjEes";
+$host = "umrahclicks.com";
+$dbname = "umrahcli_umrahclicks";
 
 date_default_timezone_set("Asia/Kuala_Lumpur");
 
-$username = "root";
-$password = "";
-$host = "localhost";
-$dbname = "umrahclicks";
+// $username = "root";
+// $password = "";
+// $host = "localhost";
+// $dbname = "umrahclicks";
 
 $conn = NEW mysqli($host,$username,$password,$dbname) or die(mysqli_error($conn));
 
