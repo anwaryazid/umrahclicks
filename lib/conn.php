458f28bb8d4090b934b10b4ca61a57729b893726
anwaryazid <m.anwaryazid@gmail.com>
2020-06-03 07:31:49 +0800

    Update conn.php

--- lib/conn.php
+++ lib/conn.php
@@ -7,20 +7,21 @@ $logoURL = "http://umrahclicks.com/staging/img/umrahclicks-logo.JPG";
 $termsURL = "http://umrahclicks.com/";
 
 // Email Configuration
-/* $mailHost = 'smtp.mailtrap.io';
+$mailHost = 'smtp.mailtrap.io';
 $mailPort = '2525';
 $mailUsername = '0bc12932211a45';
 $mailPassword = 'b79e70fdbb0e3f';
 $mailSMTPSecure = 'tls';
 $mailSetFrom = 'no-reply@umrahclicks.my';
-$mailSetFromName = 'UmrahClicks.my'; */
-$mailHost = 'smtp.gmail.com';
+$mailSetFromName = 'UmrahClicks.my';
+/* $mailHost = 'smtp.gmail.com';
 $mailPort = '465';
 $mailUsername = 'umrahclicksmy@gmail.com';
 $mailPassword = 'im6R38g7rCYTqkW';
 $mailSMTPSecure = 'ssl';
 $mailSetFrom = 'no-reply@umrahclicks.my';
-$mailSetFromName = 'UmrahClicks.my';
+$mailSetFromName = 'UmrahClicks.my'; */
+
 $mailSMTPAuth = true;
 $mailSMTPDebug = '0';
 
